0377faa195842783c95957fec69998f847cca069
danyaalqaramseh <d.alqaramseh98@gmial.com>
2021-02-16 22:23:24 +0200

    lab 8b

--- app.js
+++ app.js
@@ -1,47 +1,47 @@
-'use strict'
+// 'use strict'
 
 
-let workingHours=[ ];
+// let workingHours=[ ];
 
 
-let seattle =  { 
-    min: 0,
-    max: 0 , 
-    avg: 0 ,
+// let seattle =  { 
+//     min: 0,
+//     max: 0 , 
+//     avg: 0 ,
 
 
 
-    getmin: function () {    
-            this.min =Math.min(...this.workingHours);
-         },
-    getmax: function () {    
-        this.max = Math.max(...this.workingHours); },
+//     getmin: function () {    
+//             this.min =Math.min(...this.workingHours);
+//          },
+//     getmax: function () {    
+//         this.max = Math.max(...this.workingHours); },
 
-        getWorking : function getworkingHours(){
-                debugger;
-            for (let index = 0; index <13; index++) {
+//         getWorking : function getworkingHours(){
+//                 debugger;
+//             for (let index = 0; index <13; index++) {
                
-                this.workingHours[index] = getRandomNumber(1, 500);
-            }
+//                 this.workingHours[index] = getRandomNumber(1, 500);
+//             }
 
-        }
+//         }
 
-}
-console.log(workingHours);
+// }
+// console.log(workingHours);
 
-const hours=[ 6  ,7 , 8 , 9 , 10 , 11 , 12 , 1 , 2 , 3 , 4 ,5 , 6 , 7]; 
-const container = document.getElementById('main');
-const ulEl=document.createElement('ul');
-container.appendChild(ulEl);
-const liEl=document.createElement('li');
-ulEl.appendChild(liEl);
+// const hours=[ 6  ,7 , 8 , 9 , 10 , 11 , 12 , 1 , 2 , 3 , 4 ,5 , 6 , 7]; 
+// const container = document.getElementById('main');
+// const ulEl=document.createElement('ul');
+// container.appendChild(ulEl);
+// const liEl=document.createElement('li');
+// ulEl.appendChild(liEl);
 
-for (let index = 0; index < workingHours.length; index++) {
-   let liEl = document.createElement('li');
-    ulEl.appendchild(liEl);
-    liEl.contenttext = hours[index]+'am ' + workingHours[index] + ' cookies';
+// for (let index = 0; index < workingHours.length; index++) {
+//    let liEl = document.createElement('li');
+//     ulEl.appendchild(liEl);
+//     liEl.contenttext = hours[index]+'am ' + workingHours[index] + ' cookies';
     
-}
+// }
 
 
 
@@ -49,8 +49,8 @@ for (let index = 0; index < workingHours.length; index++) {
  
 
 
-function getRandomNumber(min, max) {
+// function getRandomNumber(min, max) {
 
 
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+// }
